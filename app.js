371815d61d891e451e9f34a833b3fc4a76fcb0a3
hailu0228 <89234174+hailu0228@users.noreply.github.com>
2025-02-13 11:23:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
-function App() {
-    const [currentPage, setCurrentPage] = React.useState('home');
-
-    const renderPage = () => {
-        switch(currentPage) {
-            case 'services':
-                return <ServicesPage />;
-            case 'about':
-                return <AboutPage onNavigate={setCurrentPage} />;
-            case 'contact':
-                return <ContactPage />;
-            default:
-                return (
-                    <div data-name="home-content">
-                        <Hero onNavigate={setCurrentPage} />
-                        <Services onNavigate={setCurrentPage} />
-                        <ServiceGallery />
-                        <Testimonials />
-                        <CustomerGallery />
-                    </div>
-                );
-        }
-    };
-
-    return (
-        <div className="app" data-name="app">
-            <Navbar onNavigate={setCurrentPage} currentPage={currentPage} />
-            {renderPage()}
-            <Footer />
-        </div>
-    );
-}
-
-try {
-    const rootElement = document.getElementById('root');
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(<App />);
-} catch (error) {
-    reportError(error);
-}
+<script>
+    document.querySelector('button').addEventListener('click', function() {
+        this.textContent = "你点过了！";
+    });
+</script>
