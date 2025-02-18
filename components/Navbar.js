ff28ac9ab8b1fb915ba42d4f06f49b8d0053ad9e
hailu0228 <89234174+hailu0228@users.noreply.github.com>
2025-02-18 11:47:17 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,8 @@ function Navbar({ onNavigate, currentPage }) {
                     >
                         {/* 替换为图片 */}
                         <img 
-                            src="images/logo.png" 
+                            // src="images/logo.png" 
+                            src="https://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E3%80%8A%E5%93%AA%E5%90%922%E3%80%8B%E5%86%B2%E9%A1%B6%E5%85%A8%E7%90%83%E5%8A%A8%E7%94%BB%E6%A6%9C&step_word=&lid=11180291191299643517&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=927343288,3881561446&os=1098592965,2650311361&simid=4227268147,553026421&pn=35&rn=1&di=46137345&ln=1088&fr=&fmq=1739850342955_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=11&spn=0&pi=0&gsm=1e&objurl=https%3A%2F%2Fq8.itc.cn%2Fimages01%2F20250218%2F773c18b74a9e42eea556793df3a4fe56.jpeg&rpstart=0&rpnum=0&adpicid=0&nojc=undefined&dyTabStr=MCwxMiwzLDEsMiwxMyw3LDYsNSw5&ctd=1739850369280^3_1825X922%1"
                             alt="COCOCOM Logo" 
                             className="h-10 w-auto mr-2 transform group-hover:rotate-12 transition-transform" 
                         />
